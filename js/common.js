9e2b5dbfb8079524b21aa57facb052ad947ddfed
asukahirata <k24.s10.t20@gmail.com>
2024-11-05 15:44:30 +0900

    top ページpcコーディング完成

--- js/common.js
+++ js/common.js
@@ -2,13 +2,13 @@
 $(window).on('load', function () {
   // load後に呼ばれる
   $(document).on('click', '.menu-trigger', function () {
-    $('.menu-trigger').toggleClass('active');
+    $('.menu-trigger span').toggleClass('active');
     $('.sp').fadeToggle();
   });
   
 });
 
 $('.gnav__menu__item a').click(function() {
-  $('.menu-trigger').toggleClass('active');
+  $('.menu-trigger span').toggleClass('active');
   $('.gnav').fadeToggle();
 });
